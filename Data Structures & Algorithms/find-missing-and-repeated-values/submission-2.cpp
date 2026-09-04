ca69e586095e44c42f4eb2429b72f3fbfecc5e78
Tom McMahon <tommymcmahon@gmail.com>
2026-09-04 18:55:32 -0500

    Add: find-missing-and-repeated-values - submission-2

---
+++ Data Structures & Algorithms/find-missing-and-repeated-values/submission-2.cpp
@@ -0,0 +1,36 @@
+#include <assert.h>
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+
+        int numElements = grid.size() * grid.size();
+
+        std::vector<int> seen(numElements);
+
+        std::cout << numElements << std::endl;
+
+        for (int i=0; i < grid.size(); ++i) {
+            for (int j=0; j < grid.size(); ++j) {
+                int value = grid[i][j];
+                seen[value-1]++;
+            }
+        }
+
+        int duplicate = 0;
+        int missing   = 0;
+
+        for (int i=0; i < numElements; ++i)
+        {
+            if (seen[i] == 0)
+            {
+                missing = i+1;   
+            }
+            else if (seen[i] == 2)
+            {
+                duplicate = i+1;
+            }
+        }
+
+        return { duplicate, missing };
+    }
+};
