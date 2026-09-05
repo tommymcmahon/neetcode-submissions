649991711866c1e9821baff712bbbb67f60092d6
Tom McMahon <tommymcmahon@gmail.com>
2026-09-05 13:15:15 -0500

    Add: count-prefix-and-suffix-pairs-i - submission-1

---
+++ Data Structures & Algorithms/count-prefix-and-suffix-pairs-i/submission-1.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int countPrefixSuffixPairs(vector<string>& words) {
+
+        int count=0;
+        for (int i = 0; i < words.size(); ++i) 
+        {
+            for (int j = i+1; j < words.size(); ++j) 
+            {
+                if (isPrefixAndSuffix(words[i], words[j]))
+                {
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+
+    bool isPrefixAndSuffix(std::string pattern, std::string word)
+    {
+        int wsize = word.size();
+        int psize = pattern.size();
+
+        if ( psize > wsize) return false;
+#if defined(DEBUG)
+        cout << "pattern: " << pattern << std::endl;
+        cout << "word: "    << word    << std::endl;
+#endif
+        for (int i=0; i < psize; ++i)
+        {
+            char p=pattern[i];
+            char l=word[i];
+            char r=word[wsize-psize+i]; 
+#if defined(DEBUG)
+            cout << "p[" << i << "]= " << p
+                 << " l=" << l << " r="  << r  
+                 << std::endl;
+#endif
+            if ((l != p) || (r != p))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
